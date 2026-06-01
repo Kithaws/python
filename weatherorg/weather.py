31d7ff09e127cad9ba9b8ea0fe4b1dafdc9042b5
itzmadaraxd <>
2026-06-01 20:10:56 +0530

    Add weatherorg weather app

---
+++ weatherorg/weather.py
@@ -0,0 +1,21 @@
+import requests
+from dotenv import load_dotenv
+import os
+from pprint import pprint
+
+load_dotenv()
+def get_current_weather(city):
+
+
+    request_url = f"https://api.openweathermap.org/data/2.5/weather?appid={os.getenv('API_KEY')}&q={city}&units=metric"
+    
+   
+    weather_data = requests.get(request_url).json()
+    return weather_data
+if __name__ == "__main__":
+    city = input("\nEnter the city name:\n")
+    weather_data = get_current_weather(city)
+
+    print("\n")
+    pprint(weather_data)
+
